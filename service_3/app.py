bb00273193d83190ac8bfc5da4b84372a0adde61
TheEarlOfGray <theearlofgray@protonmail.com>
2021-08-10 14:12:31 +0000

    EG: Quickfix

--- service_3/app.py
+++ service_3/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/get/damage')
 def get_damage():
-    return random.randint(5, 30)
+    return random.randint(1, 60)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

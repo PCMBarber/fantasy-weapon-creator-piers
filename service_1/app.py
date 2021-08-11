871518d464c2d490e71833ba4ff4f0f418cc7bd9
TheEarlOfGray <theearlofgray@protonmail.com>
2021-08-11 13:39:57 +0000

    EG: finalised app first draft, working

--- service_1/app.py
+++ service_1/app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    weapon = requests.get('http://service-2:5000/get/weapon').text
-    damage = requests.get('http://service-3:5000/get/damage').json()
+    weapon = requests.get('http://34.105.171.115:80/get/weapon').text
+    damage = requests.get('http://34.105.171.115:80/get/damage').json()
 
     content = {'weapon': weapon, 'damage': damage}
-    status = requests.post('http://service-4:5000/post/service-4', json=content).json()
+    status = requests.post('http://34.105.171.115:80/post/status', json=content).json()
 
-    return f"You generated a {weapon} with a damage multiplier of {damage} and the status effect of {status[effect]}.\n \
-        It is called the {status[level]} {status[name]} {weapon} of {status[effect]}"
+    return f"You generated a {weapon} with a damage multiplier of {damage} and the status effect of {status['effect']}.\n \
+        It is called the {status['level']} {status['name']} {weapon} of {status['effect']}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

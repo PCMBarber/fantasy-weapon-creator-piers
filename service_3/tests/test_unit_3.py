03292bbd3868e7ab4c4285df609cefc96293abc9
TheEarlOfGray <theearlofgray@protonmail.com>
2021-08-12 10:41:21 +0000

    EG: Service3 test complete

---
+++ service_3/tests/test_unit_3.py
@@ -0,0 +1,21 @@
+from flask import url_for
+from flask_testing import TestCase
+
+from service_3.app import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestResponse(TestBase):
+
+    def test_get_drink(self):
+        damage = []
+        for i in range(60):
+            damage.append(i)
+
+        for i in range(20):
+            response = self.client.get(url_for('get_damage'))
+
+            self.assert200(response)
+            self.assertIn(int(response.data.decode()), damage)

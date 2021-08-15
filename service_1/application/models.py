887542d6e9b459dddddd8958dee26e209ebf8d12
TheEarlOfGray <theearlofgray@protonmail.com>
2021-08-15 16:40:24 +0000

    Egrmbl

--- service_1/application/models.py
+++ service_1/application/models.py
@@ -6,4 +6,4 @@ class Weapon(db.Model):
     weapon_type = db.Column(db.String(20))
     damage = db.Column(db.Integer, )
     status = db.Column(db.String(20))
-    level = db.Column(db.String(2))
+    level = db.Column(db.String(20))
